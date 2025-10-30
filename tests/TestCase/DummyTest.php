fb15cfad5fe914f4dd0fbd736a70e993cbf4e163
dante di domenico <dante@didoda.com>
2025-10-30 09:38:15 +0100

    chore: DummyTest

---
+++ tests/TestCase/DummyTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BEdita\Mail\Test\TestCase;
+
+use Cake\TestSuite\TestCase;
+
+/**
+ * \BEdita\Mail Dummy Test Case
+ */
+class DummyTest extends TestCase
+{
+    public function testDummy(): void
+    {
+        $this->assertTrue(true);
+    }
+}
